74000c4c7bcd7ba3fa155d6372dc5dd93d7ae549
zawa1205 <aizu.serizawa@gmail.com>
2024-04-08 21:54:59 +0900

    シートから誕生日を判定するロジックの追加

--- src/lineBotBirthDayReminder.ts
+++ src/lineBotBirthDayReminder.ts
@@ -1,10 +1,32 @@
+import { format, addDays } from "date-fns";
+import { toZonedTime } from "date-fns-tz";
+
 export function lineBotBirthDayReminder() {
   const spreadsheetId = process.env.SPREAD_SHEET_ID;
-  const file = SpreadsheetApp.openById(spreadsheetId);
-  const sheet = file.getSheetByName("シート1");
-  const range = sheet.getRange("A1");
-  const value = range.getValue();
+  const sheetName = "シート1";
 
-  console.log(value);
-  range.setValue("ほげ");
+  const file = SpreadsheetApp.openById(spreadsheetId);
+  // シートがない場合は null が帰ってくる
+
+  const sheet = file.getSheetByName(sheetName);
+
+  // console.log(value);
+  // range.setValue("ほげ");
+  const today = toZonedTime(new Date(), "Asia/Tokyo");
+  console.log("今日", today);
+  const tomorrow = addDays(today, 1);
+  const formattedTomorrow = format(tomorrow, "MM/dd");
+  console.log("明日", formattedTomorrow);
+
+  // シートはA1はじまりなので、indexも1はじまり
+  const lastRowIndex = sheet.getLastRow();
+
+  for (let index = 1; index <= lastRowIndex; index++) {
+    const birthday = sheet.getRange(`A${index}`).getValue();
+    const formattedBirthday = format(birthday, "MM/dd");
+    const name = sheet.getRange(`B${index}`).getValue();
+    if (formattedTomorrow === formattedBirthday) {
+      console.log(name, "birthday!");
+    }
+  }
 }
